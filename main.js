12d8646c0b2baea8333e650a91d2fa02704e7d3d
Paulina Zimny <paulina.zimny@gmail.com>
2019-03-13 13:37:30 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const image = document.querySelector("img.slider");
+const h1 = document.querySelector("h1.slider");
+const dots = [...document.querySelectorAll(".dots span")];
+const slideList = [
+  {
+    img: "images/img1.jpg",
+    text: "First banner info"
+  },
+  { img: "images/img2.jpg", text: "Second banner info" },
+  { img: "images/img3.jpg", text: "Third banner info" }
+];
+
+const time = 4000;
+let active = 0;
+
+const changeDot = () => {
+  const activeDot = dots.findIndex(dot => dot.classList.contains("active"));
+  dots[activeDot].classList.remove("active");
+  dots[active].classList.add("active");
+};
+
+const changeSlide = () => {
+  active++;
+  if (active === slideList.length) {
+    active = 0;
+  }
+  image.src = slideList[active].img;
+  h1.textContent = slideList[active].text;
+  changeDot();
+};
+
+let intervalIndex = setInterval(changeSlide, time);
+
+const keyChangeSlide = e => {
+  if (e.keyCode == 37 || e.keyCode == 39) {
+    clearInterval(intervalIndex);
+    e.keyCode == 37 ? active-- : active++;
+    if (active === slideList.length) {
+      active = 0;
+    } else if (active < 0) {
+      active = slideList.length - 1;
+    }
+    image.src = slideList[active].img;
+    h1.textContent = slideList[active].text;
+    changeDot();
+    intervalIndex = setInterval(changeSlide, time);
+  }
+};
+
+window.addEventListener("keydown", keyChangeSlide);
